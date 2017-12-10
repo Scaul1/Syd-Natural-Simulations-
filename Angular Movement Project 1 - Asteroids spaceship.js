f680d7790c3f743030acde74c0601bf4ef658d50
Sydney Caulfeild <31681144+Scaul1@users.noreply.github.com>
2017-12-10 16:45:49 -0500

    Update Angular Movement Project 1 - Asteroids spaceship.js
    Finished project.

--- Angular Movement Project 1 - Asteroids spaceship.js
+++ Angular Movement Project 1 - Asteroids spaceship.js
@@ -1,47 +1,60 @@
+//use the left and right arrow keys to turn the spaceship and press the z key to speed up
+
+//based on code from "Turning car" in Khan Academy
+
+//changing angle mode to radians instead of degrees
 angleMode = "radians";
 
-//function declaring properties of balls
+//function declaring properties of spaceship
 var Spaceship = function() {
     this.position = new PVector(width/2, height/2);
-    this.velocity = new PVector(0, 0);
+    this.velocity = new PVector(0, -0.5);
     this.acceleration = new PVector(0, 0);
-    this.turn = new PVector (0, 0);
     this.color = color(random(255), random(255), random(255), 127);
-    this.topspeed = 4;
-    this.xoff = 10500;
+    this.topspeed = 6;
+    this.xoff = 1000;
     this.yoff = 0;
     this.r = 10;
 };
 
-//updating position of ball by changing acceleration, velocity and position
+//applying the forces to change acceleration
+Spaceship.prototype.applyForce = function(force) {
+    this.acceleration.add(force);
+};
+
+//function to rotate the spaceship to the left
+Spaceship.prototype.rotateLeft = function(){
+    var left = this.velocity.get();
+    left.rotate(-PI/4);
+    this.applyForce(left);
+};
+
+//function to rotate the spaceship to the right
+Spaceship.prototype.rotateRight = function(){
+    var right = this.velocity.get();
+    right.rotate(PI/4);
+    this.applyForce(right);
+};
+
+//function to speed up the spaceship
+Spaceship.prototype.speedUp = function(){
+    var force = this.velocity.get();
+    force.normalize();
+    force.mult(2);
+    this.applyForce(force);
+};
+
+//increasing speed by adding acceleration, and changing the position using the new speed
 Spaceship.prototype.update = function() {
     this.velocity.add(this.acceleration);
     this.velocity.limit(this.topspeed);
     this.position.add(this.velocity);
-    //this.acceleration.mult(0);
-};
-
-//applying the forces to change acceleration
-Spaceship.prototype.applyForce = function(force) {
-    //this.acceleration.add(force);
-};
-
-Spaceship.prototype.rotateLeft = function(){
-    var left = PVector.get(this.velocity);
-    left.rotate(-PI/10);
-    this.applyForce(left);
-};
-
-Spaceship.prototype.rotateRight = function(){
-    var right = PVector.get(this.velocity);
-    right.rotate(PI/10);
-    this.applyForce(right);
+    this.acceleration.mult(0);
 };
 
 //displaying the spaceship
 Spaceship.prototype.display = function() {
     var angle = this.velocity.heading();
-    println(angle);
     
     stroke(0);
     strokeWeight(2);
@@ -49,7 +62,7 @@ Spaceship.prototype.display = function() {
     
     pushMatrix();
     translate(this.position.x, this.position.y);
-    //rotate(angle);
+    rotate(angle + PI/2);
     triangle(20, 100, 60, 100, 40, 57);
     rect(24, 100, 10, 8);
     rect(44, 100, 10, 8);
@@ -57,35 +70,32 @@ Spaceship.prototype.display = function() {
     popMatrix();
 };
 
-
-//making the balls bounce off the sides of the canvas
+//making the spaceship stay on the cavnas
 Spaceship.prototype.checkEdges = function() {
     var xForce = 0;
     var yForce = 0;
-    //if ball goes off canvas on the right
+    //if spaceship goes off canvas on the right
     if (this.position.x > width) {
         this.position.x = 0;
     }
-    //else if ball goes off canvas on the left
+    //else if spaceship goes off canvas on the left
      else if (this.position.x < 0) {
         this.position.x = width;
     }
-    //if ball goes off canvas on bottom
+    //if spaceship goes off canvas on bottom
     if (this.position.y > height) {
         this.position.y = 0;
     }
-    //else if the ball goes off the canvas on the top
+    //else if the spaceship goes off the canvas on the top
      else if (this.position.y < 0) {
         this.position.y = height;
     }
-    //returning new bouncing force
-    return new PVector (xForce, yForce);
-    
 };
 
+//creating new spaceship variable to be updated
 var spaceship = new Spaceship();
 
-
+//calls different movement functions depending on which key is being pressed
 keyPressed = function(){
     if (keyCode === LEFT){
         spaceship.rotateLeft();
@@ -93,26 +103,18 @@ keyPressed = function(){
     else if (keyCode === RIGHT){
         spaceship.rotateRight();
     }
+    //90 is the z key
+    else if (keyCode === 90){
+        spaceship.speedUp();
+    }
     
 };
-
 
 draw = function() {
     background(159, 190, 242);
     
-    if (keyIsPressed && keyCode === UP){
-        spaceship.acceleration.set(0, -0.05);
-    }
-    else {
-        while (spaceship.velocity.y > 0)
-        {
-            spaceship.acceleration.set(0, 0.1);
-        }
-        spaceship.acceleration.set(0, 0);
-    }
-    
-    //applying all of the forces then updating and displaying the balls in the array
-    spaceship.applyForce(spaceship.checkEdges());
+    //updating and displaying the spaceship and checking it's edges to keep it on the canvas
     spaceship.update();
-    spaceship.display();    
+    spaceship.display();
+    spaceship.checkEdges();
 };
