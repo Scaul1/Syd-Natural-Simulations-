774d45d0c417914304a003d71ed1d7c5d995b344
Sydney Caulfeild <31681144+Scaul1@users.noreply.github.com>
2017-12-18 09:43:37 -0500

    Create Particle Systems Project 1 - Creature Colonies.js

---
+++ Particle Systems Project 1 - Creature Colonies.js
@@ -0,0 +1,5 @@
+-particle system of rain that falls from clouds user makes
+
+-particle system of little flies
+-inheritance of this particle system that is a different fly 
+-all flies avoid ladybugs
